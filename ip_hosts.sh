dd8108ea7d882fd059d1f2405c271eb1604d4bd8
topotrix <topotrix@gmail.com>
2012-11-28 13:35:34 +0800

    new

--- ip_hosts.sh
+++ ip_hosts.sh
@@ -9,7 +9,7 @@ current_dir=$(pwd)
 cp -i /etc/hosts $current_dir/hosts_backup
 sed -e '/whatbrowser/d' -e '/blogger/d' -e '/doubleclick/d' -e '/youtube/d' -e '/gstatic/d' -e '/gmail/d' -e '/google/d' $current_dir/hosts_backup > /etc/hosts
 hkip=`nslookup -vc www.google.com.hk 8.8.8.8 | tail -n 2 | grep "Address" | awk '{print $2}'`
-for serv in `cat $current_dir/google_domains.txt`; do
+for serv in `cat $current_dir/domains_ip_hosts.txt`; do
         echo "$hkip $serv" >> /etc/hosts
 done
 
