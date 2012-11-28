4507d72d7649f572d17f2aeb82439edc32e1a615
topotrix <topotrix@gmail.com>
2012-11-28 13:31:01 +0800

    add google domains and modify ip_hosts.sh

--- ip_hosts.sh
+++ ip_hosts.sh
@@ -6,9 +6,8 @@
 #!/bin/bash
 
 current_dir=$(pwd)
-echo 'backup /etc/hosts in current directory with filename "hosts_backup"'
 cp -i /etc/hosts $current_dir/hosts_backup
-sed '/google/d' $current_dir/hosts_backup > /etc/hosts
+sed -e '/whatbrowser/d' -e '/blogger/d' -e '/doubleclick/d' -e '/youtube/d' -e '/gstatic/d' -e '/gmail/d' -e '/google/d' $current_dir/hosts_backup > /etc/hosts
 hkip=`nslookup -vc www.google.com.hk 8.8.8.8 | tail -n 2 | grep "Address" | awk '{print $2}'`
 for serv in `cat $current_dir/google_domains.txt`; do
         echo "$hkip $serv" >> /etc/hosts
